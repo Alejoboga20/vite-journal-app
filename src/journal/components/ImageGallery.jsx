66dd1b9f343554e06aba535361233a809db9bb66
Alejo Boga <alejoboga20@gmail.com>
2022-07-12 15:44:15 -0300

    Image gallery

--- src/journal/components/ImageGallery.jsx
+++ src/journal/components/ImageGallery.jsx
@@ -1,3 +1,13 @@
-export const ImageGallery = () => {
-	return <div>ImageGallery</div>;
+import { ImageList, ImageListItem } from '@mui/material';
+
+export const ImageGallery = ({ itemData = [] }) => {
+	return (
+		<ImageList sx={{ width: '100%', height: 450 }} cols={4} rowHeight={200}>
+			{itemData.map((item) => (
+				<ImageListItem key={item.img}>
+					<img loading='lazy' src='' />
+				</ImageListItem>
+			))}
+		</ImageList>
+	);
 };
