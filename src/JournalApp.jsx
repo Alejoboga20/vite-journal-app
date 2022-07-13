8b707b892d0c7279f129320622d4cd09d7110d90
Alejo Boga <alejoboga20@gmail.com>
2022-07-13 16:04:20 -0300

    Configure redux

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+
 import { AppRouter } from './router/AppRouter';
 import { AppTheme } from './theme';
+import { store } from './store';
 
 export const JournalApp = () => {
 	return (
-		<BrowserRouter>
-			<AppTheme>
-				<AppRouter />
-			</AppTheme>
-		</BrowserRouter>
+		<Provider store={store}>
+			<BrowserRouter>
+				<AppTheme>
+					<AppRouter />
+				</AppTheme>
+			</BrowserRouter>
+		</Provider>
 	);
 };
