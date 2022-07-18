48f816c5d6a7ec03e0d43bfe5132e313484dbc95
Alejo Boga <alejoboga20@gmail.com>
2022-07-18 13:04:42 -0300

    Set Active notes

--- src/journal/components/SideBarItem.jsx
+++ src/journal/components/SideBarItem.jsx
@@ -1,16 +1,32 @@
 import { useMemo } from 'react';
+import { useDispatch } from 'react-redux';
 import { TurnedInOutlined } from '@mui/icons-material';
 import { Grid, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import { setActiveNote } from '../../store/journal';
 
-export const SideBarItem = ({ title = '', body = '' }) => {
+export const SideBarItem = ({ title = '', body = '', id, date, imageUrls = [] }) => {
+	const dispatch = useDispatch();
+
 	const newTitle = useMemo(
 		() => (title.length > 13 ? title.substring(0, 13) + '...' : title),
 		[title]
 	);
 
+	const onSelectNote = () => {
+		dispatch(
+			setActiveNote({
+				title,
+				body,
+				id,
+				date,
+				imageUrls,
+			})
+		);
+	};
+
 	return (
 		<ListItem>
-			<ListItemButton>
+			<ListItemButton onClick={onSelectNote}>
 				<ListItemIcon>
 					<TurnedInOutlined />
 				</ListItemIcon>
