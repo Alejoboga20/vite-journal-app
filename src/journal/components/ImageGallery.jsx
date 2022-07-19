645fce8a1e945380ef409359d0561bfa2ed2f620
Alejo Boga <alejoboga20@gmail.com>
2022-07-19 10:21:31 -0300

    Show images

--- src/journal/components/ImageGallery.jsx
+++ src/journal/components/ImageGallery.jsx
@@ -1,11 +1,16 @@
 import { ImageList, ImageListItem } from '@mui/material';
 
-export const ImageGallery = ({ itemData = [] }) => {
+export const ImageGallery = ({ images = [] }) => {
 	return (
 		<ImageList sx={{ width: '100%', height: 450 }} cols={4} rowHeight={200}>
-			{itemData.map((item) => (
-				<ImageListItem key={item.img}>
-					<img loading='lazy' src='' />
+			{images.map((image) => (
+				<ImageListItem key={image}>
+					<img
+						loading='lazy'
+						src={`${image}?w=164&h164&fit+crop&auto=format`}
+						srcSet={`${image}?w=164&h164&fit+crop&auto=format&dpr=2 2x`}
+						alt='Note Image'
+					/>
 				</ImageListItem>
 			))}
 		</ImageList>
