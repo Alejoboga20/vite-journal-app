2a2413252308b1f77b9fe845949e85b12d70bac3
Alejo Boga <alejoboga20@gmail.com>
2022-07-19 12:29:14 -0300

    Delete image after test

--- src/tests/helpers/uploadFile.test.js
+++ src/tests/helpers/uploadFile.test.js
@@ -1,4 +1,12 @@
+import { v2 as cloudinary } from 'cloudinary';
 import { uploadFile } from '../../helpers';
+
+cloudinary.config({
+	cloud_name: 'finastra',
+	api_key: '245889453199165',
+	api_secret: 'bS-xIf2Qts5ZxixbHLRPETnmJvA',
+	secure: true,
+});
 
 const imageUrl =
 	'https://www.adorama.com/alc/wp-content/uploads/2018/11/landscape-photography-tips-yosemite-valley-feature.jpg';
@@ -10,7 +18,18 @@ describe('Test uploadFile', () => {
 		const file = new File([blob], 'test.jpg');
 
 		const url = await uploadFile(file);
-		console.log({ url });
+		const segments = url.split('/');
+		const imageId = segments[segments.length - 1].replace('.jpg', '');
+
 		expect(typeof url).toBe('string');
+
+		await cloudinary.api.delete_resources([imageId]);
+	});
+
+	test('should return null', async () => {
+		const file = new File([], 'test.jpg');
+		const url = await uploadFile(file);
+
+		expect(url).toBe(null);
 	});
 });
