9a4060f01d4c77dc0dcb186683b38e8fb00fcdaf
Alejo Boga <alejoboga20@gmail.com>
2022-07-20 12:17:29 -0300

    Test more auth thunks

--- src/tests/store/auth/thunks.test.js
+++ src/tests/store/auth/thunks.test.js
@@ -1,6 +1,16 @@
-import { singInWithGoogle } from '../../../firebase/providers';
+import {
+	loginInWithEmailAndPassword,
+	logoutFirebase,
+	singInWithGoogle,
+} from '../../../firebase/providers';
 import { checkingCredentials, login, logout } from '../../../store/auth/authSlice';
-import { checkingAuthentication, startGoogleSignIn } from '../../../store/auth/thunks';
+import {
+	checkingAuthentication,
+	startGoogleSignIn,
+	startLoginWithEmailPassword,
+	startLogout,
+} from '../../../store/auth/thunks';
+import { clearNotesLogout } from '../../../store/journal';
 import { demoUser } from '../../fixtures/authFixtures';
 
 jest.mock('../../../firebase/providers.js');
@@ -31,5 +41,35 @@ describe('Auth Thunks Tests', () => {
 		await startGoogleSignIn()(dispatch);
 		expect(dispatch).toHaveBeenCalledWith(checkingCredentials());
 		expect(dispatch).toHaveBeenCalledWith(logout(logoutData));
+	});
+
+	test('should startLoginWithEmailAndPassword should call checkingCredentials and login', async () => {
+		const { email, password = 'TestingTest1234!' } = demoUser;
+		const loginData = { ok: true, ...demoUser };
+
+		await loginInWithEmailAndPassword.mockResolvedValue(loginData);
+		await startLoginWithEmailPassword(email, password)(dispatch);
+
+		expect(dispatch).toHaveBeenCalledWith(checkingCredentials());
+		expect(dispatch).toHaveBeenCalledWith(login(demoUser));
+	});
+
+	test('should startLoginWithEmailAndPassword should call checkingCredentials and logout with error', async () => {
+		const { email, password = 'WrongPassword' } = demoUser;
+		const logoutData = { ok: false, message: 'Wrong Password' };
+
+		await loginInWithEmailAndPassword.mockResolvedValue(logoutData);
+		await startLoginWithEmailPassword(email, password)(dispatch);
+
+		expect(dispatch).toHaveBeenCalledWith(checkingCredentials());
+		expect(dispatch).toHaveBeenCalledWith(logout({ message: 'Wrong Password' }));
+	});
+
+	test('should call logoutFireBase, clearNotes and logout', async () => {
+		await startLogout()(dispatch);
+
+		expect(logoutFirebase).toHaveBeenCalled();
+		expect(dispatch).toHaveBeenCalledWith(clearNotesLogout());
+		expect(dispatch).toHaveBeenCalledWith(logout({}));
 	});
 });
