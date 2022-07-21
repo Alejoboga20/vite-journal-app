56ae2903f56804b30939b4d46eee903bbefed537
Alejo Boga <alejoboga20@gmail.com>
2022-07-21 10:55:51 -0300

    Use Env variables

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,28 +4,22 @@ import { getFirestore } from 'firebase/firestore/lite';
 
 import { getEnvironments } from '../helpers';
 
-const env = getEnvironments();
+const {
+	VITE_APIKEY,
+	VITE_AUTHDOMAIN,
+	VITE_PROJECTID,
+	VITE_STORAGEBUCKET,
+	VITE_MESSAGINGSENDERID,
+	VITE_APPID,
+} = getEnvironments();
 
-console.log({ env });
-
-//Development
-/* const firebaseConfig = {
-	apiKey: 'AIzaSyDXVqhGgojjbfiy3_pdabOBt8fPvhS8wIU',
-	authDomain: 'react-tutorials-11879.firebaseapp.com',
-	projectId: 'react-tutorials-11879',
-	storageBucket: 'react-tutorials-11879.appspot.com',
-	messagingSenderId: '48038583513',
-	appId: '1:48038583513:web:642198a748485180a772c8',
-}; */
-
-//Testing
 const firebaseConfig = {
-	apiKey: 'AIzaSyAdb07ZA7sQVJYe6q544bzuetnUMV-wFxg',
-	authDomain: 'react-tutorial-tests.firebaseapp.com',
-	projectId: 'react-tutorial-tests',
-	storageBucket: 'react-tutorial-tests.appspot.com',
-	messagingSenderId: '817496311383',
-	appId: '1:817496311383:web:1e461ba0870a028e600512',
+	apiKey: VITE_APIKEY,
+	authDomain: VITE_AUTHDOMAIN,
+	projectId: VITE_PROJECTID,
+	storageBucket: VITE_STORAGEBUCKET,
+	messagingSenderId: VITE_MESSAGINGSENDERID,
+	appId: VITE_APPID,
 };
 
 // Initialize Firebase
